c982aebfcc6e43c015bd69711c346b660320ef2d
danaithadra <daha025@student.kristiania.no>
2025-02-18 13:22:04 +0100

    added some dummy users in user in flyway

---
+++ backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email, password) VALUES
+                                              ('testuser1', 'test1@email.com', 'password123'),
+                                              ('testuser2', 'test2@email.com', 'password123');
