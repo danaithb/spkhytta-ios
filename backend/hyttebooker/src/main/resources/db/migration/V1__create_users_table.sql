10747d6114588a811fdaa1c8b778d90d7a4f57e5
danaithadra <daha025@student.kristiania.no>
2025-02-18 14:40:02 +0100

    changed userid datatype from long to int

--- backend/hyttebooker/src/main/resources/db/migration/V1__create_users_table.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,6 +1,7 @@
 create table if not exists users (
-    userId int auto_increment primary key,
+    user_id int auto_increment primary key,
     name varchar(255) not null ,
     email varchar(255) unique not null,
-    password varchar(255) not null
+    password varchar(255) not null,
+    firebase_uid VARCHAR(255) UNIQUE
 );
