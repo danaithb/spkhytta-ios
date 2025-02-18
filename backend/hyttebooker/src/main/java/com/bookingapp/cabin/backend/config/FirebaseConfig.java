d4487f57fdc6e565876c632f5667979179ecc6af
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-18 12:07:18 +0100

    made firebase config file

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.bookingapp.cabin.backend.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+//source:https://stackoverflow.com/questions/44185432/firebase-admin-sdk-with-java/47247539#47247539
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public void initializeFirebase() throws IOException {
+        if (FirebaseApp.getApps().isEmpty())
+            try(InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-adminsdk.json")) {
+                if (serviceAccount == null) {
+                    throw new IOException("Kunne ikke finne firebase-adminsdk.json.");
+                }
+
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            }
+
+    }
+}
