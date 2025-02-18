5c897302357b36a0ff65def4a2fce449ceded926
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-18 11:21:27 +0100

    Initial commit

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/CabinBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bookingapp.cabin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinBookingAppApplication.class, args);
+	}
+
+}
---
+++ backend/hyttebooker/src/test/java/com/bookingapp/cabin/backend/CabinBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingapp.cabin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
