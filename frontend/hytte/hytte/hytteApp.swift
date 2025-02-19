4b560a38e7958e4d998d67bf36221f31714033f8
Jana737 <111773364+Jana737@users.noreply.github.com>
2025-02-19 19:49:57 +0100

    first commit on login-ui

---
+++ frontend/hytte/hytte/hytteApp.swift
@@ -0,0 +1,17 @@
+//
+//  hytteApp.swift
+//  hytte
+//
+//  Created by Jana Carlsson on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct hytteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
