4c32e91b17e31fd629e492e6381a7bdc66749379
Jana737 <111773364+Jana737@users.noreply.github.com>
2025-02-21 11:50:01 +0100

    Adding SplashScreen
    Adding splash-screen view

---
+++ frontend/hytte/hytte/Views/SplashScreenView/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  hytte
+//
+//  Created by Jana Carlsson on 21/02/2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
