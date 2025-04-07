88920ad539520a37af2e1b8fae2fca8732e1d88c
Abigail <112098949+agquaye@users.noreply.github.com>
2025-04-07 15:16:38 +0200

    UI til profileView side
    satt opp ui må wrap med mine booking

--- Spkhytta-iOS/Views/ProfileView/ProfileView.swift
+++ Spkhytta-iOS/Views/ProfileView/ProfileView.swift
@@ -7,11 +7,82 @@
 
 import SwiftUI
 
+
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                // Profil Side
+                Text("Min Side")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top)
+                
+                VStack{
+                    //Profil Bilder og Navn
+                    Image(systemName:"person.circle")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .padding()
+                }
+                
+                VStack  {
+                    Text("Ola Nordaman")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    
+                    Text("It-Utvikler")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                .padding()
+                
+                VStack{
+                    Text("Mine Booking")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .padding()
+                    
+                    
+                }
+                        
+                HStack{
+                    Text("Dato: 01.01.205")
+                    Spacer()
+                    Text("Anatall personer: 4")
+                }
+                .padding()
+                
+                VStack{
+                    HStack{
+                        Text("Status på Booking")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    
+                    HStack{
+                        Text("Bekreftet")
+                        Circle()
+                            .fill(Color.green)
+                            .frame(width: 10, height: 10)
+                    } 
+                    
+                }
+            }
+        }
+                    
     }
 }
+
+
+
+
+
+
+
+
 
 #Preview {
     ProfileView()
