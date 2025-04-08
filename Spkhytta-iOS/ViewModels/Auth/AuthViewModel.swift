e1def28e651a5f1ad1890c843d8c84fb57dc8bc1
Jana737 <111773364+Jana737@users.noreply.github.com>
2025-04-08 14:19:49 +0200

    comment
    try for merge conflict

--- Spkhytta-iOS/ViewModels/Auth/AuthViewModel.swift
+++ Spkhytta-iOS/ViewModels/Auth/AuthViewModel.swift
@@ -12,6 +12,7 @@
 //lägg till guard på login
 
 //till danial. bör vi tex ha en auth manager för api request även om det bara ska användas här när det gäller token? ska lägga överallt när man gör kall till backend för att verifiera.
+//added to try
 
 import SwiftUI
 import Firebase
