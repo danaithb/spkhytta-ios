a9ab106ab86727a7b38ed98bd0bf3051aa4ad367
danaithb <daha025@student.kristiania.no>
2025-04-29 12:35:15 +0200

    added model folder and userfile for userfinfo
    used for tables in db

---
+++ Spkhytta-iOS/Models/UserInfo.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfo.swift
+//  Spkhytta-iOS
+//
+//  Created by Danait Hadra on 29/04/2025.
+//
+
+import Foundation
+
+struct UserInfo: Codable {
+    let name: String
+    let email: String
+    let points: Int
+}
