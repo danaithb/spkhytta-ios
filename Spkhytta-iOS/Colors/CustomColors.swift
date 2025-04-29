da4be4250e1d7ac12a9a7d59eb46385ce17b71e3
Jana737 <111773364+Jana737@users.noreply.github.com>
2025-04-29 16:50:28 +0200

    Custom Colors
    added custom colors in assets and CustomColors code

--- Spkhytta-iOS/Colors/CustomColors.swift
+++ Spkhytta-iOS/Colors/CustomColors.swift
@@ -11,6 +11,10 @@ import SwiftUI
 extension Color {
     static let custom_blue = Color("customBlue")
     static let red_days = Color("redDays")
-    static let borders_grey = Color("borders")
-
+    static let borders_grey = Color("bordersGrey")
+    static let confirmed_green = Color("confirmedGreen")
+    static let pending_yellow = Color("pendingYellow")
+    static let blue_date = Color("blueDate")
+    static let canceled_grey = Color("canceledGrey")
+    static let blocked_red = Color("blockedRed")
 }
